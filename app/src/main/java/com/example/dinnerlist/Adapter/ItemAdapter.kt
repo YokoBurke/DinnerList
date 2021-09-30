ce4e89ce91fe391e6a83e43cd006391fa1b75e58
YokoBurke <fujikurock@gmail.com>
2021-09-30 15:44:32 -0400

    Added intent and menu items

--- app/src/main/java/com/example/dinnerlist/Adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/dinnerlist/Adapter/ItemAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.dinnerlist.Adapter
 
 import android.content.Context
+import android.content.Intent
+import android.content.res.Resources
 import android.graphics.Color
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,10 +19,14 @@ class ItemAdapter (private val context: Context,
                    private val dataset: List<MyDinner>)
     :RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
 
-    //nexted class
-    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
-    val textView: TextView = view.findViewById(R.id.item_title)
-    val imageView: ImageView = view.findViewById(R.id.item_image)
+    companion object {
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    //nested class
+    class ItemViewHolder(val view: View): RecyclerView.ViewHolder(view){
+        val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -32,11 +39,21 @@ class ItemAdapter (private val context: Context,
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        holder.textView.text = context.resources.getString(item.stringResrouceId)
+        //
+        // holder.textView.text = context.resources.getString(item.stringResrouceId)
+        val menuName = context.resources.getString(item.stringResrouceId)
+        holder.textView.text = menuName
         holder.imageView.setImageResource(item.imageREsourceId)
 
         val thisColor = selectColor()
         holder.textView.setTextColor(Color.parseColor(thisColor))
+
+        holder.textView.setOnClickListener{
+            val queryUrl: Uri = Uri.parse(SEARCH_PREFIX + menuName)
+            val context = holder.view.context
+            val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/dinnerlist/MainActivity.kt
+++ app/src/main/java/com/example/dinnerlist/MainActivity.kt
@@ -3,18 +3,69 @@ package com.example.dinnerlist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dinnerlist.Adapter.ItemAdapter
 import com.example.dinnerlist.Data.Datasource
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private var isLinearLayoutManager = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val myDataset = Datasource().loadDinner()
-        val recyclerView = findViewById<RecyclerView>(R.id.my_recycler)
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler)
         recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
+    }
+
+    private fun chooseLayout() {
+        if (isLinearLayoutManager){
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(this, 3)
+        }
+
+    }
+
+    private fun setIcon(menuItem: MenuItem?){
+        if (menuItem == null)
+            return
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+            ContextCompat.getDrawable(this, R.drawable.list_view)
+        else ContextCompat.getDrawable(this, R.drawable.grid_view)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                isLinearLayoutManager = !isLinearLayoutManager
+                chooseLayout()
+                setIcon(item)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
